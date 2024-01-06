4b4cb0e9ec14b7ceb503925cdde08ca49147933e
seoulstudent <59911052+seoulstudent@users.noreply.github.com>
2024-01-06 15:57:08 +0900

    Initial commit

---
+++ module/utils/src/utils.cpp
@@ -0,0 +1,5 @@
+#include "utils/utils.hpp"
+
+#include <iostream>
+
+void Print(const std::string& message) { std::cout << message << std::endl; }
---
+++ src/main.cpp
@@ -0,0 +1,4 @@
+#include <cstdint>
+#include <iostream>
+
+int32_t main() { std::cout << "Hello World" << std::endl; }
