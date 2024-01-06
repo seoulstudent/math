8d3e39f7789dc3e0da455153e60f9d39b358e3d6
seoulstudent <gingtom2@naver.com>
2024-01-06 16:41:32 +0900

    Added skelton fies for math utils

---
+++ include/math/utils.hpp
@@ -0,0 +1,12 @@
+// Copyrights (c) 2023 seoulstudent, All right reserved.
+// Authors : seoulstudent
+
+#ifndef SEOULSTDENT__MATH__UTILS_HPP_
+#define SEOULSTDENT__MATH__UTILS_HPP_
+
+namespace seoulstudent::math {
+
+
+}
+
+#endif
---
+++ src/utils..cpp
@@ -0,0 +1,9 @@
+// Copyrights (c) 2023 seoulstudent, All right reserved.
+// Authors : seoulstudent
+
+#include "math/utils.hpp"
+
+#include <cstdint>
+#include <iostream>
+
+int32_t main() { std::cout << "Hello World" << std::endl; }
