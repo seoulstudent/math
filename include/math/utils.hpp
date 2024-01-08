e3fc3500916979138ba31c6caaac1348821f24ff
seoulstudent <gingtom2@naver.com>
2024-01-08 20:01:09 +0900

    Added Subtract, Multiply, Divide math functions

--- include/math/utils.hpp
+++ include/math/utils.hpp
@@ -5,10 +5,12 @@
 #define SEOULSTDENT__MATH__UTILS_HPP_
 
 namespace seoulstudent::math {
+  auto Add(double lhs, double rhs) -> double;
+  auto Subtract(double lhs, double rhs) -> double;
+  auto Multiply(double lhs, double rhs) -> double;
+  auto Divide(double lhs, double rhs) -> double;
 
-    
-
-
+  auto IsEqual(double lhs, double rhs) -> bool;
 } // seoustudent
 
 #endif
--- src/utils.cpp
+++ src/utils.cpp
@@ -4,8 +4,51 @@
 #include "math/utils.hpp"
 
 #include <cmath>
+#include <limits>
 #include <stdexcept>
+#include <tuple>
 
 namespace seoulstudent::math {
 
-}  
+auto Add(double lhs, double rhs) -> double {
+  if (std::isnan(lhs) || std::isnan(rhs) || std::isinf(lhs) ||
+      std::isinf(rhs)) {
+    throw std::invalid_argument("rhs or lhs is nan or inf");
+  }
+  return lhs + rhs;
+}
+
+auto Subtract(double lhs, double rhs) -> double {
+  if (std::isnan(lhs) || std::isnan(rhs) || std::isinf(lhs) ||
+      std::isinf(rhs)) {
+    throw std::invalid_argument("rhs or lhs is nan or inf");
+  }
+  return lhs - rhs;
+}
+
+auto Multiply(double lhs, double rhs) -> double {
+  if (std::isnan(lhs) || std::isnan(rhs) || std::isinf(lhs) ||
+      std::isinf(rhs)) {
+    throw std::invalid_argument("rhs or lhs is nan or inf");
+  }
+  return lhs * rhs;
+}
+
+auto Divide(double lhs, double rhs) -> double {
+  if (std::isnan(lhs) || std::isnan(rhs) || std::isinf(lhs) ||
+      std::isinf(rhs) || (rhs == 0)) {
+    throw std::invalid_argument("rhs or lhs is nan or inf or rhs is zero");
+  }
+  return lhs / rhs;
+}
+
+auto IsEqual(double lhs, double rhs) -> bool {
+  return (std::abs(lhs - rhs) < std::numeric_limits<double>::epsilon());
+}
+
+auto IsEqual(float lhs, float rhs) -> bool {
+  return (std::abs(lhs - rhs) < std::numeric_limits<double>::epsilon());
+}
+
+// namespace seoulstudent::math
+}  // namespace seoulstudent::math
